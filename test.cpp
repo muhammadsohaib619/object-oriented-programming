c487db8e12f1227fc4242cdee9b85aabdcb318c4
Sohaib <muhammadsohaibsk619@gmail.com>
2026-08-04 01:59:35 +0500

    add demo person class to show how oop works

--- test.cpp
+++ test.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
 using namespace std;
 
+class Person {
+public:
+    string name;
+    int age;
+    
+};
+
+
+
 int main() {
 
-cout << "hello world";
+    cout << "hello world"<<endl;  
+    cout << "we will learn classes and objects"<<endl;  
+    Person p1;
+    p1.name = "sohaib";
+    p1.age = 21;
+    cout << "name: "<< p1.name << endl;
+    cout << "age: "<< p1.age << endl;
 
 return 0;
 }
