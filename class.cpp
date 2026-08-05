1009543f058d92676ca62867093fac475f873dc9
Sohaib <muhammadsohaibsk619@gmail.com>
2026-08-05 23:47:44 +0500

    madea function getInfo which display info of person

--- class.cpp
+++ class.cpp
@@ -14,8 +14,19 @@ public:
     void setName(string name){
         this->name = name;
     }
+    void setAge(int age){
+        this->age = age;
+    }
     string getName(){
         return name;
+    }
+    int getAge(){
+        return age;
+    }
+    void getInfo(){
+        cout<<"---------Person Info-----------"<<endl;
+        cout<<"name: "<< name <<endl;
+        cout<<"age: "<< age <<endl;        
     }
 };
 
@@ -27,7 +38,8 @@ int main() {
     //added getter and setters functions we keep attribute private and allow limited access through getters and setters funtion
     p1.setName("sohaib");
     p1.getName();
-
+    p1.setAge(21);
+    p1.getInfo();
 
 return 0;
 }
