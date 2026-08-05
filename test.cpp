d70d315050bb8f4b3065ead875d84d6287b2704b
Sohaib <muhammadsohaibsk619@gmail.com>
2026-08-05 23:31:16 +0500

    add a constructur

--- test.cpp
+++ test.cpp
@@ -5,7 +5,10 @@ class Person {
 public:
     string name;
     int age;
-    
+    Person(string name, int age){
+        this->name = name;
+        this->age = age;
+    }
 };
 
 
@@ -14,9 +17,11 @@ int main() {
 
     cout << "hello world"<<endl;  
     cout << "we will learn classes and objects"<<endl;  
-    Person p1;
-    p1.name = "sohaib";
-    p1.age = 21;
+    // Person p1;
+    // p1.name = "sohaib";
+    // p1.age = 21;
+    //now with the help constructor we can initialized person's attributes
+    Person p1("sohaib", 21);
     cout << "name: "<< p1.name << endl;
     cout << "age: "<< p1.age << endl;
 
