916592969fe1fa80c0a24762aa74afc492cff486
Sohaib <muhammadsohaibsk619@gmail.com>
2026-08-11 00:09:14 +0500

    add a basic code to show inheritance

--- inheritance/inherit.cpp
+++ inheritance/inherit.cpp
@@ -2,8 +2,40 @@
 using namespace std;
 
 class person{
+protected:
+    string name;
+    int age;
+public:
+    person() { }
+    person(string name, int age){
+        this->name = name;
+        this->age = age;
+    }
+    void setName(string name){
+        this->name = name;
+    }
+    void setAge(int age){
+        this->age = age;
+    }
+    string getName(){
+        return name;
+    }
+    int getAge(){
+        return age;
+    }
+    void getInfo(){
+        cout<<"---------Person Info-----------"<<endl;
+        cout<<"name: "<< name <<endl;
+        cout<<"age: "<< age <<endl;        
+    }
+};
+
+class student : public person {
 private:
-    
+    string stuId;
+    double gpa;
+public:
+
 };
 
 int main() {
